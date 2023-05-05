448493893bdfdf590567a6e1372567abe3d8c53d
Amit Kumar <amitkr2410@gmail.com>
2023-05-05 16:31:34 -0400

    deploy nltk model

---
+++ NLP_twitter_aws/app.py
@@ -0,0 +1,48 @@
+import pickle
+import os
+import sys
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import uvicorn
+from mangum import Mangum
+import src.custom_function as customfunc
+
+##Load saved model pickle files
+filename = 'preprocess/CountVectorizer.pickle'
+vectorizer = pickle.load( open(filename,'rb') )
+
+filename = 'eval/finalmodel.pickle'
+classifier = pickle.load( open(filename,'rb') )
+
+##### find the result for a given test sms
+def predict_result(MyTwitterMessage):
+    sms = [customfunc.text_process(MyTwitterMessage)]
+    x_sms = vectorizer.transform(sms)
+    ans = classifier.predict(x_sms)
+    if ans ==-1:
+        output = 'The message is Negative'
+    if ans == 0:
+        output = 'The message is Neutral'
+    if ans == 1:
+        output ='The message is Positive'
+    print(output)
+    return output 
+#####
+
+##Create flaskAPI instance
+app = FastAPI()
+handler=Mangum(app)
+
+@app.get('/')
+#def read_root():
+#    return {'Hello':'world'}
+
+def NLP_twitter_sentiment(text:str):
+    pred=text
+    #print('The entered text is ', text)
+    #pred=predict_result(text)
+    return JSONResponse( {'The sentiment of the entered text is ':pred} )
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=9000)
+
---
+++ NLP_twitter_aws/command.sh
@@ -0,0 +1,20 @@
+aws ecr get-login-password --region us-east-1 | docker login --username AWS \
+ --password-stdin 131162195726.dkr.ecr.us-east-1.amazonaws.com
+
+docker build -t aws-private .
+docker tag aws-private:latest 131162195726.dkr.ecr.us-east-1.amazonaws.com/aws-private:latest
+docker push 131162195726.dkr.ecr.us-east-1.amazonaws.com/aws-private:latest
+
+
+aws lambda create-function \
+    --role arn:aws:iam::131162195726:role/service-role/my_test-role-camlbqlc   \
+    --function-name my_test \
+    --package-type Image \
+    --region us-east-1 \
+    --code ImageUri=131162195726.dkr.ecr.us-east-1.amazonaws.com/aws-private:latest
+    --architecture 
+
+aws lambda update-function-code \
+     --function-name my_test \
+     --region us-east-1 \
+     --image-uri 131162195726.dkr.ecr.us-east-1.amazonaws.com/aws-private:latest
