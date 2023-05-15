2a1e58cba24ba2436a93fae55d5179bc9a8273f0
Amit Kumar <amitkr2410@gmail.com>
2023-05-15 14:07:01 -0400

    initialize dvc svc project

---
+++ dvc_pipelines_svc/command.sh
@@ -0,0 +1,7 @@
+
+
+
+#Add remote
+dvc remote add -d storage gdrive://0AIac4JZqHhKmUk9PDA
+dvc remote modify myremote gdrive_client_id 'client-id'
+dvc remote modify myremote gdrive_client_secret 'client-secret'
